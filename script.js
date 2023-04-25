b35faa749c43e102278c203da342f0b4118a6a40
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2023-04-25 10:10:41 -0300

    API

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var x = "everything everywhere all at once";
 fetch(
-  "http://www.omdbapi.com//?t=" +
-    x +
-    "&i=<string>&s=<string>&y=<integer>&type=<string>&plot=short&tomatoes=false&r=json&v=1&page=1&callback=<string>"
+  "https://api.themoviedb.org/3/movie/top_rated?api_key=d4f3a21a5ab99ecd653b548b11bcc686&language=pt-BR&page=1"
 )
   .then((response) => response.json())
   .then((data) => {
     console.log(data);
+    for (let i = 0; i < data; i++) {
+      console.log(i.poster_path);
+    }
   })
   .catch((error) => {
     console.log("api: " + error);
