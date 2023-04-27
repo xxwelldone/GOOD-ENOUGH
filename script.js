ae624f80f8f85a8c9a074d9015539816e8be93cf
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2023-04-27 12:41:43 -0300

    API funcionando

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-var x = "everything everywhere all at once";
-fetch(
+const api = fetch(
   "https://api.themoviedb.org/3/movie/top_rated?api_key=d4f3a21a5ab99ecd653b548b11bcc686&language=pt-BR&page=1"
-)
+);
+const conteudo = document.querySelectorAll("#movie h3");
+const imagens = document.querySelectorAll("#movie .Image");
+const paragraph = document.querySelectorAll(".paragraph");
+console.log(imagens);
+//console.log(conteudo);
+api
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    for (let i = 0; i < data; i++) {
-      console.log(i.poster_path);
+    for (let i = 0; i < conteudo.length; i++) {
+      conteudo[i].innerText = data.results[i].original_title;
+      imagens[i].src =
+        "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
+
+      paragraph[i].innerText = data.results[i].overview;
+
+      /* console.log(
+        "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path
+      );*/
     }
-  })
-  .catch((error) => {
-    console.log("api: " + error);
+
+    //conteudo.innerText = data.results[0].original_title;
   });
