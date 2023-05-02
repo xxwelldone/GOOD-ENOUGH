944525b297662377429976ca793e96f059ad9c89
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2023-05-02 15:17:37 -0300

    Update on header nav structure

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ const paragraph = document.querySelectorAll(".paragraph");
 const filmesHome = document.querySelectorAll("#movieIndex h3");
 const imagensHome = document.querySelectorAll("#movieIndex .Image");
 const score = document.querySelectorAll(".score");
+const thumb = document.querySelectorAll(".thumb");
 
+const search = document.querySelector(".search");
+
+/* API for Film List */
 api
   .then((response) => response.json())
   .then((data) => {
@@ -26,9 +30,13 @@ api
     // }
   });
 
+/*Get API Poster, title and Score/change color for home page */
 const apiHome = fetch(
-  "https://api.themoviedb.org/3/search/movie?api_key=d4f3a21a5ab99ecd653b548b11bcc686&language=pt-BR&query=(xuxa)"
+  "https://api.themoviedb.org/3/movie/popular?api_key=d4f3a21a5ab99ecd653b548b11bcc686&language=en-US&page=2"
 );
+//api.themoviedb.org/3/movie/popular?api_key=d4f3a21a5ab99ecd653b548b11bcc686&language=en-US&page=
+
+//https: "https://api.themoviedb.org/3/search/movie?api_key=d4f3a21a5ab99ecd653b548b11bcc686&language=pt-BR&query=(xuxa)";
 
 apiHome
   .then((response) => response.json())
@@ -39,34 +47,30 @@ apiHome
         "https://image.tmdb.org/t/p/w500" + data.results[i].poster_path;
       if (data.results[i].vote_average > 6) {
         score[i].innerText = data.results[i].vote_average;
-        score[i].style.color = "#0ed308";
+        score[i].style.color = "var(--cor-p1)";
+        thumb[i].style.color = "var(--cor-p1)";
+        thumb[i].style.rotate = "0deg";
       } else if (data.results[i].vote_average <= 6) {
         score[i].innerText = data.results[i].vote_average;
-        score[i].style.color = "#e705dc";
+        score[i].style.color = "var(--cor-p3)";
+        thumb[i].style.color = "var(--cor-p3)";
+        thumb[i].style.rotate = "180deg";
       } else {
         score[i].innerText = "Unavailable";
       }
     }
   });
+/* API for Seachr bar */
 
-// const barra = document.querySelector(".input");
-// barra.addEventListener("keydown", (event) => {
-//   if (event.key === "Enter") {
-//     var search = document.querySelectorAll(".search").values;
-
-//     console.log(search.trim());
-//   }
-// });
-
-// console.log(search);
-
-// const pesquisa = fetch(
-//   "https://api.themoviedb.org/3/search/movie?api_key=d4f3a21a5ab99ecd653b548b11bcc686&language=pt-BR&query=(" +
-//     search +
-//     ")"
-// );
-// pesquisa
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log(data);
-//   });
+const pesquisa = fetch(
+  "https://api.themoviedb.org/3/search/movie?api_key=d4f3a21a5ab99ecd653b548b11bcc686&language=pt-BR&query=(" +
+    search +
+    ")"
+);
+pesquisa
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      console.log(data.results[i].original_title);
+    }
+  });
